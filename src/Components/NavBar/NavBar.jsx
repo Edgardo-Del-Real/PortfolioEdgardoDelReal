c323dbf5052b267d00d732623f6b92f14c9bc196
edgardo <edgardodelreal@gmail.com>
2025-03-24 18:10:54 -0300

    pequeño desarrollo del nav

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,7 +4,13 @@ import './NavBar.css'
 const navBar = () =>{
     return(
       <>
-        
+         <nav class="navbar d-flex justify-content-center align-items-center p-2 mt-4 ">
+        <ul class="d-flex list-unstyled m-0 border rounded-5">
+        <li class="p-2"><a href="">HOME</a></li>
+        <li class="p-2"><a href="">ABOUT ME</a></li>
+        <li class="p-2"><a href="">SKILLS</a></li>
+    </ul>
+</nav>
       </>
     )
 }
