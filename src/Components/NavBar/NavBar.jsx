eb3fedd6c1a6ffa48a21e78345329eb4a1b1a29e
edgardo <edgardodelreal@gmail.com>
2025-03-25 10:29:30 -0300

    Instalacion de bootstrap y avances del nav

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,14 +5,15 @@ const navBar = () =>{
     return(
       <>
          <nav class="navbar d-flex justify-content-center align-items-center p-2 mt-4 ">
-        <ul class="d-flex list-unstyled m-0 border rounded-5">
-        <li class="p-2"><a href="">HOME</a></li>
-        <li class="p-2"><a href="">ABOUT ME</a></li>
-        <li class="p-2"><a href="">SKILLS</a></li>
+        <ul class="d-flex list-unstyled m-0 border rounded-5 p-2 ">
+        <li class="pt-2 px-5 "><a href="" class='	'>HOME</a></li>
+        <li class="p-2 px-5"><a href="">ABOUT ME</a></li>
+        <li class="p-2 px-5"><a href="">SKILLS</a></li>
+        <li class="p-2 px-5"><a href="">CONTACTO</a></li>
     </ul>
 </nav>
       </>
-    )
+    ) 
 }
 
 export default navBar
