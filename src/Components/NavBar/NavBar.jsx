3991d3aefa09e0e0ac05b796a7c193e165a955b2
edgardo <edgardodelreal@gmail.com>
2025-03-25 20:22:32 -0300

    disposicion del nav terminado. Sin funcionalidades ni estilos

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,16 +4,17 @@ import './NavBar.css'
 const navBar = () =>{
     return(
       <>
-         <nav class="navbar d-flex justify-content-center align-items-center p-2 mt-4 ">
-            <ul class="d-flex list-unstyled m-0 border rounded-5 p-2 ">
-              <li class="pt-2 px-5 "><a href="" class='	'>HOME</a></li>
-              <li class="p-2 px-5"><a href="">ABOUT ME</a></li>
-              <li class="p-2 px-5"><a href="">SKILLS</a></li>
-            </ul>
-            <button type="button" class="btn btn-success rounded-5">
-              CONTACT ME
-            </button>
-          </nav>
+        <nav class=" d-flex justify-content-between align-items-center navbar p-2 mt-4 me-2 ms-2">
+            <section class="d-flex flex-grow-1 justify-content-center ">
+                <ul class="d-flex list-unstyled m-0 borde rounded-5 p-2 bg-dark bg-opacity-50">
+                  <li class="pt-2 px-5 ms-2"><a href="#">HOME</a></li>
+                  <li class="p-2 px-5 ms-2"><a href="#">ABOUT ME</a></li>
+                  <li class="p-2 px-5 ms-2"><a href="#">SKILLS</a></li>
+                </ul>
+            </section>
+            <button type="button" class="bg-dark bg-opacity-50 borde rounded-5 p-3">CONTACT ME</button>
+        </nav>
+
       </>
     ) 
 }
