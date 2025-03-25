e9192ec5788f3ffb89ce6900798dd7afb8e04978
edgardo <edgardodelreal@gmail.com>
2025-03-25 19:06:30 -0300

    actualizacion

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,13 +5,15 @@ const navBar = () =>{
     return(
       <>
          <nav class="navbar d-flex justify-content-center align-items-center p-2 mt-4 ">
-        <ul class="d-flex list-unstyled m-0 border rounded-5 p-2 ">
-        <li class="pt-2 px-5 "><a href="" class='	'>HOME</a></li>
-        <li class="p-2 px-5"><a href="">ABOUT ME</a></li>
-        <li class="p-2 px-5"><a href="">SKILLS</a></li>
-        <li class="p-2 px-5"><a href="">CONTACTO</a></li>
-    </ul>
-</nav>
+            <ul class="d-flex list-unstyled m-0 border rounded-5 p-2 ">
+              <li class="pt-2 px-5 "><a href="" class='	'>HOME</a></li>
+              <li class="p-2 px-5"><a href="">ABOUT ME</a></li>
+              <li class="p-2 px-5"><a href="">SKILLS</a></li>
+            </ul>
+            <button type="button" class="btn btn-success rounded-5">
+              CONTACT ME
+            </button>
+          </nav>
       </>
     ) 
 }
