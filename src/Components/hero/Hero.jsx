2d78c40ad7d37c7a767d304fdaf53011608fa1b9
edgardo <edgardodelreal@gmail.com>
2025-04-02 10:38:59 -0300

    cambios

--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -6,14 +6,14 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 const Hero = () => {
     return (
         <>
-            <div className="card container " style={{ maxWidth: '540px' }}>
+            <div className="container card bg-secondary " style={{ maxWidth: '540px' }}>
                 <div className="row g-5">
                     <div className="col-md-4">
                         <img src={fotoMia} className="img-fluid rounded-start" alt="Edgardo Del Real" />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h5 className="card-title d-flex justify-content-center">EDGARDO DEL REAL</h5>
+                            <h5 className="card-title d-flex justify-content-center text-white">EDGARDO DEL REAL</h5>
                             <p className="card-text d-flex justify-content-center">Estudiante de Ingenieria en Sistemas</p>
                             <p className="card-text d-flex justify-content-center">FullStack Developer</p>
                             <section className='d-flex justify-content-center'>
